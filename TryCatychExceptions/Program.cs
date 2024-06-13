5d9b4c328ca24bb4021f66cc589cfcc319f75efb
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-13 22:30:29 +0200

    13.06

--- TryCatychExceptions/Program.cs
+++ TryCatychExceptions/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace TryCatychExceptions
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TryCatchExceptions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> Prices = new Dictionary<string, int>();
+            try 
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    argument(); 
+                }
+
+                //var pricesarr = Prices.ToArray();
+                //var outOfRange = pricesarr[pricesarr.Length];
+
+                string notExistingKey = null;
+                int value = Prices[notExistingKey];
+
+                ReadOnlyCollection<string, int> readonlyPrices = new ReadOnlyCollection<string, int>(Prices);
+
+
+
+            }
+            //catch (IndexOutOfRangeException)
+            //{
+            //    Console.WriteLine("Argument out of range exception here");
+            //}
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Argument null exception here");
+            } 
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Argument exception here");
+            }
+            
+            
+
+
+            void argument()
+            {
+                string itemName = Console.ReadLine();
+                int price = int.Parse(Console.ReadLine());
+
+                addItem(itemName, price); 
+
+                void addItem(string itemName, int price)
+                {
+                    Prices.Add(itemName, price);
+                }
+            }
         }
     }
 }
