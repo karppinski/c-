6e81296f4349f55a7285a7f873164b77bafb641d
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-17 08:39:20 +0200

    17.06

---
+++ PatternsNAttributes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PatternsNAttributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsJanetOrJohn(string name) =>
+                name.ToUpper() is var upper && (upper == "JANNET" || upper == "JOHN");
+
+            string GetWeightCategory(decimal bmi) => bmi switch
+            {
+                < 18.5m => "underweight",
+                < 25m => "normal",
+                < 30m => "overwieght",
+                _ => "obese"
+            };
+
+            bool IsVowel(char c) => c is 'a' or 'e' or 'i' or 'o' or 'u';
+
+            bool Bettween1And9(int n) => n is >= 1 and <= 9;
+
+            bool IsLetter(char c) => c is >= 'a' and <= 'z'
+                                       or >= 'A' and <= 'Z';
+
+
+            bool ShouldAllow(Uri uri) => uri switch
+            {
+                { Scheme: "http", Port: 80 } => true,
+                { Scheme.Length : 4, Port: 80 } => true,
+                { Host.Length : < 1000, Port: >0 } => true,
+
+                { Scheme: "https", Port: 443 } => true,
+                { Scheme: "ftp", Port: 21 } => true,
+                { IsLoopback: true } => true,
+                _ => false
+            };
+
+
+
+        }
+    }
+}
---
+++ TuplesRecords/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace TuplesRecords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = ("Bob", 23);
+
+            var joe = bob;
+            joe.Item1 = "Joe";
+
+            Console.WriteLine(bob);
+            Console.WriteLine(joe);
+            Console.ReadKey();
+
+            (string, int) john = ("John", 20);
+
+            (string, int) person = GetPerson();
+            Console.WriteLine(person.Item1);
+            Console.WriteLine(person.Item2);
+
+            (string, int) GetPerson() => ("Bob", 23);
+
+            // deconstructing
+
+            var rob = ("Rob", 50);
+
+            //string name = rob.Item1;
+            //int age = rob.Item2;
+
+            (string name, int age) = bob;
+            (string name, int age) bobo= bob;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+            Console.ReadKey();
+
+
+            var tuple1 = ("one", 1);
+            var tuple2 = ("one", 1);
+
+            Console.WriteLine(tuple1.Equals(tuple2));
+            Console.WriteLine(tuple1 == tuple2);
+
+            Console.ReadKey();
+
+            Console.WriteLine(tuple1.GetHashCode());
+            Console.WriteLine(tuple2.GetHashCode());
+
+            Console.ReadKey();
+
+            Point p1 = new Point(2, 3);
+            Point p2 = p1 with { A = 10 }; // nondestructive mutatiuon, 
+
+            Poooint p3 = new Poooint(2, 3);
+            Poooint p4 = p3 with { C = double.NaN };
+        }
+
+        record struct Pooint
+        {
+
+        }
+
+        record Point 
+        {
+            public Point(int a, int b) => (A, B) = (a, b);
+
+            public int A { get; init; }
+            public int B { get; init; }
+        }
+
+        record Poooint
+        {
+            public Poooint(double c, double d) => (C, D) = (c, d);
+
+            double _x;
+            public double C
+            {
+                get => _x;
+                init
+                {
+                    if (double.IsNaN(value))
+                    {
+                        throw new ArgumentException("X Cannot be NaN");
+                    }
+                    _x = value;
+                }
+            }
+
+            public double D { get; init; }
+        }
+
+        record Student ( string ID, string LastName, string GivenName)
+        {
+            public string Id { get; } = ID;
+
+            readonly int _enrollmentYear = int.Parse(ID.Substring(0, 4));
+        }
+
+        
+    }
+}
