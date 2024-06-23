8847defeb1c93a0d0724adf8f6f6a452faad6782
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-23 18:27:40 +0200

    23.06

--- Equity/Program.cs
+++ Equity/Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace Equity
 {
+
     internal class Program
     {
+        public static bool AreEqual(object obj1, object obj2)
+                   => obj1 == null ? obj2 == null : obj1.Equals(obj2);
         static void Main(string[] args)
         {
             var dt1 = new DateTimeOffset(2010, 1, 1, 1, 1, 1, TimeSpan.FromHours(8));
@@ -29,6 +32,42 @@
             var s2 = "http://" + "www.jan.com";
 
             Console.WriteLine(s1 == s2); //overloaded operators of uri and string lets them act as value types
+
+            Console.WriteLine(s1.Equals(s2));
+            Console.WriteLine(object.ReferenceEquals(s1, s2) + "references");
+            /* while interpolating strings if they are the same, they are stored in one place in memory thats 
+             * why code above returns true */
+
+            var s3 = new string("http://www.jan.com".ToCharArray());
+
+            Console.WriteLine(object.ReferenceEquals(s1, s3) + " s1 to s3 references");
+            Console.WriteLine(object.ReferenceEquals(s1, s3) + " s2 to s3 references");
+
+            var s4 = new string(new char[] { 'j', 'a', 'n' });
+            var s5 = new string(new char[] { 'j', 'a', 'n' });
+
+            // its from string class not char array 
+            Console.WriteLine(object.ReferenceEquals(s5, s4) + " char array references");
+
+            string s6 = null;
+            bool eq = s1 == s6;
+            Console.WriteLine(eq +  "  this can take null but equals not ");
+
+            //Console.WriteLine(s6.Equals(s1));
+
+            Console.WriteLine(AreEqual(s1,s2));
+            Console.WriteLine(AreEqual(s1,s6));
+
+            Console.WriteLine(object.Equals(s1,s2));
+
+
+            double x = double.NaN;
+            Console.WriteLine("comparing double nan " + (x == x));
+            Console.WriteLine(x.Equals(x));
+
+
+
+
         }
     }
 
@@ -36,4 +75,7 @@
     {
         public int X;
     }
+
+
+
 }
