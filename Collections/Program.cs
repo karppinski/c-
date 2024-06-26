ed283613c82e6aef5435cd5a26d43f5e816aba07
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-26 12:53:40 +0200

    26.06

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -67,6 +67,11 @@ namespace Collections
             object copierdAr;
 
 
+            float[] reals = { 1.3f, 1.5f, 1.8f };
+            int[] wholes = Array.ConvertAll(reals, r => Convert.ToInt32(r)); // creates new array and copying over elements 
+
+
+
             
 
 
