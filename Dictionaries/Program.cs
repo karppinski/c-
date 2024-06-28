e0bbf83bbf3d51167427cc6f4a1adc5f0699b59b
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-28 12:47:55 +0200

    28.06

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Dictionaries
+﻿using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace Dictionaries
 {
     internal class Program
     {
@@ -22,12 +25,47 @@
 
 
             // its because looking up for hashed key is O(1) and do not need to check for whole dictionary..
-            
 
 
+
+            var sorted = new SortedList<string, MethodInfo>();
+
+            foreach (MethodInfo m in typeof(object).GetMethods())
+                sorted[m.Name] = m;
+
+            foreach(string name in sorted.Keys)
+                Console.WriteLine(name);
+
+            foreach(MethodInfo m in sorted.Values)
+                Console.WriteLine(m.Name + " returns a " + m.ReturnType);
 
 
             Console.WriteLine("Hello, World!");
+
+
+            Test t = new Test();
+            Console.WriteLine(t.Namez.Count);
+            t.AddInternally();
+
+            Console.WriteLine(t.Namez.Count);
+
+            //  t.Namez.Add("test");  compiler error
+
+            ((IList<string>)t.Namez).Add("test");
+
+Console.WriteLine(t.Namez.Count);
+
         }
     }
+
+
+    public class Test
+    {
+        List<string> namez = new List<string>();
+        public IReadOnlyList<string> Namez => namez;
+
+        public void AddInternally() => namez.Add("test");
+
+    }
+
 }
