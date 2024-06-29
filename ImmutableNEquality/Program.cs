35a7c7ac1a3f1ede37cb032cd2397a0093c74388
karppinski <krzysztof.karpinski135@gmail.com>
2024-06-29 21:52:56 +0200

    29.06/2

--- ImmutableNEquality/Program.cs
+++ ImmutableNEquality/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace ImmutableNEquality
 {
@@ -34,6 +35,39 @@ namespace ImmutableNEquality
             ImmutableArray<int> myImmutable = builder.ToImmutable();
 
 
+            Customer c1 = new Customer("Bloggs", "Joe");
+            Customer c2 = new Customer("Bloggs", "Joe");
+
+            Console.WriteLine(c1 == c2);
+            Console.WriteLine(c1.Equals(c2));
+
+            //var d = new Dictionary<Customer, string>();
+
+            //d[c1] = "Joe";
+            //Console.WriteLine(d.ContainsKey(c2));
+
+            var eqComparer = new LastfirstEqComparer();
+            
+            var d = new Dictionary<Customer, string>(eqComparer);
+
+            d[c1] = "Joe";
+            Console.WriteLine(d.ContainsKey(c2));
+
+            var dic = new SortedDictionary<string, string>(new SurnameComparer());
+            dic.Add("MacPhail", "second!");
+            dic.Add("MacWilliam", "third!");
+            dic.Add("McDonald", "first!");
+
+            foreach(string s in dic.Values)
+                Console.Write(s + " ");
+
+            var dict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            string[] names = { "Tom", "HARRY", "sheila" };
+            CultureInfo ci = new CultureInfo("en-AU");
+            Array.Sort<string>(names, StringComparer.Create(ci, false));
+
+            foreach(string name in names)
+                Console.WriteLine(name);
 
 
 
@@ -61,6 +95,22 @@ namespace ImmutableNEquality
         public override int GetHashCode(Customer obj)
             => (obj.LastName + ";" + obj.FirstName).GetHashCode();
        
+    }
+
+    public class SurnameComparer : Comparer<string>
+    {
+        string Normalize(string s)
+        {
+            s = s.Trim().ToUpper();
+            if (s.StartsWith("MC")) s = "MAC" + s.Substring(2);
+            return s;
+        }
+
+        public override int Compare(string? x, string? y)
+        {
+            return Normalize(x).CompareTo(Normalize(y));
+
+        }
     }
 
 }
