0c0119ca24fce4db424ce83c41a317712d2e6746
karppinski <krzysztof.karpinski135@gmail.com>
2024-07-06 12:55:46 +0200

    06.07

--- LINQ Queries/Program.cs
+++ LINQ Queries/Program.cs
@@ -55,6 +55,74 @@
             IEnumerable<int> lastTwo = numbers.Skip(numbers.Length - 2);
 
             IEnumerable<int> reversed = numbers.Reverse();
+
+            int secondNumber = numbers.ElementAt(1);
+            int secondLowest = numbers.OrderBy(n => n).Skip(1).First();
+
+
+            IEnumerable<string> sortedByLength, sortedAlphabetically;
+            sortedByLength = namez.OrderBy(n => n.Length);// int key
+            sortedAlphabetically = namez.OrderBy(n => n); //string key 
+
+
+            int[] seq1 = { 1, 2, 3 };
+            int[] seq2 = { 3, 4, 5 };
+
+
+            IEnumerable<int> concat = seq1.Concat(seq2);
+            IEnumerable<int> union = seq1.Union(seq2);
+
+            foreach(int i in concat) Console.WriteLine(i + ", concat");
+            foreach(int i in union) Console.WriteLine(i + ", union");
+
+
+
+            int matches = (from n in namez where n.Contains("a") select n).Count();
+            int matches1 = namez.Where(n => n.Contains("a")).Count();
+            Console.WriteLine("matches: " + matches +" "+  matches1);
+
+            string first = (from n in namez orderby n select n).First();
+            string first1 = namez.OrderBy(n => n).First();
+            Console.WriteLine("first: " + first + " " + first1);
+
+
+            var numbersX = new List<int> { 1 };
+
+            IEnumerable<int> query5 = numbersX.Select(n => n * 10); // build query
+
+            numbersX.Add(2);
+
+            foreach(int n in query5) // executed only when enumerated !!!
+                Console.Write(n + " | ");
+
+            Console.WriteLine();
+            var numbers5 = new List<int> { 1, 2 };
+
+            IEnumerable<int> query66 = numbers5.Select(n => n * 10);
+
+            foreach (int n in query66) Console.Write(n + " | ");
+
+            numbers5.Clear();
+            Console.WriteLine();
+
+
+            foreach (int n in query66) Console.Write(n + " | "); // nothing happend !!!!
+            Console.WriteLine(  );
+
+            var numbers6 = new List<int> { 1, 2 };
+
+            IEnumerable<int> query67 = numbers6.Select(n => n * 10).ToList(); /// need to assign it to list to copy values !!
+
+            foreach (int n in query67) Console.Write(n + " | ");
+            Console.WriteLine();
+            numbers6.Clear();
+
+            Console.WriteLine();
+            foreach (int n in query67) Console.Write(n + " | ");
+
+
+
+
         }
     }
 }
