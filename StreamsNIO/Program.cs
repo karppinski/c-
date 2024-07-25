d4b091b77c88dec0d153109b1be44d8c20169687
karppinski <krzysztof.karpinski135@gmail.com>
2024-07-25 08:39:29 +0200

    25.07

---
+++ StreamsNIO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+
+namespace StreamsNIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Stream s = new FileStream("test.txt", FileMode.Create))
+            {
+                Console.WriteLine(s.CanRead);
+                Console.WriteLine(s.CanSeek);
+                Console.WriteLine(s.CanWrite);
+
+                s.WriteByte(101);
+                s.WriteByte(102);
+                byte[] block = { 1, 2, 3, 4, 5 };
+                s.Write(block, 0, block.Length);
+
+                Console.WriteLine(s.Length);
+                Console.WriteLine(s.Position);
+                s.Position = 0;
+
+                Console.WriteLine(s.ReadByte());
+                Console.WriteLine(s.ReadByte());
+
+                Console.WriteLine(s.Read(block, 0, block.Length));
+
+                Console.WriteLine(s.Read(block, 0, block.Length));
+
+            }
+
+            FileStream f1 = File.OpenRead("readme.bin"); // read only
+            FileStream f2 = File.OpenWrite("writeme.tmp"); // write only
+            FileStream f3 = File.Create("readWrite.tmp"); // read and write 
+
+        }
+
+        async static void AsyncDemo()
+        {
+            using (Stream s = new FileStream("test.txt", FileMode.Create))
+            {
+                
+                byte[] block = { 1, 2, 3, 4, 5 };
+
+                await s.WriteAsync(block, 0, block.Length);
+
+                s.Position = 0;
+
+                Console.WriteLine( await s.ReadAsync(block, 0, block.Length));
+            }
+        }
+    }
+}
--- awaits/Program.cs
+++ awaits/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            async Task PrintAnswer()
+            {
+                await Task.Delay(5000);
+                int answer = 21 * 2;
+                Console.Out.WriteLine(answer);
+            }
+
+             async Task Go()
+            {
+                await PrintAnswer();
+                Console.WriteLine("Done");
+            }
+
+            Go();
         }
+
+
+
     }
 }
