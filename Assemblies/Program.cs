aa183ee1bd640ffd5eb8d1e95a18aae73af6c7b6
karppinski <krzysztof.karpinski135@gmail.com>
2024-08-01 08:26:45 +0200

    01.08

---
+++ Assemblies/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Assemblies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = typeof(Program).Assembly;
+
+            Console.WriteLine(a.GetName());
+            Console.WriteLine(a.FullName);
+
+
+
+        }
+    }
+}
