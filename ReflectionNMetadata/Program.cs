55fae08770f3cf36ec93950aedc204671fa3d3bd
karppinski <krzysztof.karpinski135@gmail.com>
2024-08-06 08:25:18 +0200

    06.08

--- ReflectionNMetadata/Program.cs
+++ ReflectionNMetadata/Program.cs
@@ -35,6 +35,49 @@ namespace ReflectionNMetadata
             gen.EmitWriteLine("Hello World!");
             gen.Emit(OpCodes.Ret);
             dynMeth.Invoke(null, null);
+
+
+
+            var dynMeth2 = new DynamicMethod("Foo2", null, null, typeof(Program));
+            ILGenerator gen2 = dynMeth2.GetILGenerator();
+            MethodInfo writeLineInt = typeof(Console).GetMethod("WriteLine", new Type[] { typeof(int) });         
+            
+            // 10 / 2 + 1
+            gen2.Emit(OpCodes.Ldc_I4, 10);
+            gen2.Emit(OpCodes.Ldc_I4, 2);
+            gen2.Emit(OpCodes.Div);
+            gen2.Emit(OpCodes.Ldc_I4, 1);
+            gen2.Emit(OpCodes.Add);
+            gen2.Emit(OpCodes.Call, writeLineInt);
+            gen2.Emit(OpCodes.Ret);
+            dynMeth2.Invoke(null, null);
+
+            int x = 6;
+            int y = 7;
+            x *= y;
+            Console.WriteLine(x);
+
+            var dynMeth3 = new DynamicMethod("Test", null, null, typeof(void));
+            ILGenerator gen3 = dynMeth3.GetILGenerator();
+
+            LocalBuilder localX = gen3.DeclareLocal(typeof(int));
+            LocalBuilder localY = gen3.DeclareLocal(typeof(int));
+
+            gen3.Emit(OpCodes.Ldc_I4, 6);
+            gen3.Emit(OpCodes.Stloc, localX);
+            gen3.Emit(OpCodes.Ldc_I4, 7);
+            gen3.Emit(OpCodes.Stloc, localY);
+
+            gen3.Emit(OpCodes.Ldloc, localX);
+            gen3.Emit(OpCodes.Ldloc, localY);
+            gen3.Emit(OpCodes.Mul);
+            gen3.Emit(OpCodes.Stloc, localX);
+
+            gen3.EmitWriteLine(localX);
+            gen3.Emit(OpCodes.Ret);
+
+            dynMeth3.Invoke(null,null);
+
         }
 
 
---
+++ crypto/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace crypto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = { 1, 2, 3, 4, 5 };
+
+            //using (var rsa = new RSACryptoServiceProvider(2048))
+            //{
+            //    byte[] encrypted = rsa.Encrypt(data, true);
+            //    byte[] decrypted = rsa.Decrypt(encrypted, true);
+            //}
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                File.WriteAllText("PublicKeyOnly.xml", rsa.ToXmlString(false));
+                File.WriteAllText("PublicPrivate.xml", rsa.ToXmlString(true));
+            }
+
+
+
+            string publicKeyOnly = File.ReadAllText("PublicKeyOnly.xml");
+            string publicPrivate = File.ReadAllText("PublicPrivate.xml");
+
+
+            byte[] data1 = Encoding.UTF8.GetBytes("Message to encrypt");
+
+            byte[] encrypted, decrypted;
+
+            using (var rsaPublicOnly = new RSACryptoServiceProvider())
+            {
+                rsaPublicOnly.FromXmlString(publicKeyOnly);
+                encrypted = rsaPublicOnly.Encrypt(data1, true);
+                // this will throw exception because you need the private key in order to decrypt
+                //  decrypted = rsaPublicOnly.Decrypt(encrypted, true);
+            }
+
+            using (var rsaPublicPrivate = new RSACryptoServiceProvider())
+            {
+                // With the private key we can successfully decrypt
+
+                rsaPublicPrivate.FromXmlString(publicPrivate);
+                decrypted = rsaPublicPrivate.Decrypt(encrypted, true);
+            }
+
+
+
+            //byte[] key = new byte[16];
+            //byte[] iv = new byte[16];
+
+            //var cryptoRng = RandomNumberGenerator.Create();
+            //cryptoRng.GetBytes(key);
+            //cryptoRng.GetBytes(iv);
+
+            //byte[] data = Encoding.UTF8.GetBytes("Yeah!");
+
+            //string encrypted = Encrypt(data, key, iv);
+            //Console.WriteLine(encrypted);
+
+            //string decrypted = Decrypt(encrypted, key, iv);
+            //Console.WriteLine(decrypted);
+
+
+
+
+        }
+
+        //public static byte[] Encrypt(byte[] data, byte[] key, byte[] iv)
+        //{
+        //    using Aes algorithm = Aes.Create();
+        //    algorithm.Key = key;
+        //    return algorithm.EncryptCbc(data, iv);
+        //}
+
+        //public static byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        //{
+        //    using Aes algorithm = Aes.Create();
+        //    algorithm.Key = key;
+        //    return algorithm.DecryptCbc(data, iv);
+        //}
+
+        //public static string Encrypt(string data, byte[] key, byte[] iv)
+        //{
+        //    byte[] encryptedBytes = EncryptToBytes(Encoding.UTF8.GetBytes(data), key, iv);
+        //    return Convert.ToBase64String(encryptedBytes);
+        //}
+
+
+    }
+}
