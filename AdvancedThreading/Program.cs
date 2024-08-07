adb078c3a96aabde25220539013f2bd55b89414c
karppinski <krzysztof.karpinski135@gmail.com>
2024-08-07 10:33:21 +0200

    07.08

---
+++ AdvancedThreading/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace AdvancedThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            
+        }
+    }
+
+    class ThreadUnsafe
+    {
+        static int _vali1 = 1, _vali2 = 1;
+
+        static void Go()
+        {
+            if(_vali2 != 0) Console.WriteLine(_vali1 / _vali2);
+            _vali2 = 0;
+        }
+    }
+
+    class ThreadSafe
+    {
+        static readonly object _locker = new object();   
+        static int _vali1 = 1, _vali2 = 1;
+
+        
+        static void Go()
+        {
+            lock (_locker)
+            {
+                if (_vali2 != 0) Console.WriteLine(_vali1 / _vali2);
+                _vali2 = 0;
+            }
+
+            Monitor.Enter(_locker);
+            try
+            {
+           
+                if (_vali2 != 0) Console.WriteLine(_vali1 / _vali2);
+                _vali2 = 0;
+            }
+            finally { Monitor.Exit(_locker); }
+        }
+    }
+
+    //class Deadlock
+    //{
+    //    object locker1 = new object();
+    //    object locker2 = new object();
+
+    //    new Thread(() =>{
+    //        lock (locker1)
+    //        {
+    //            Thread.Sleep(1000);
+    //            lock (locker2) ;
+    //        }
+    //    }).Start(); 
+
+    //    lock(locker2){
+    //        Thread.Sleep(1000);
+    //        lock(locker1);
+    //}
+}
