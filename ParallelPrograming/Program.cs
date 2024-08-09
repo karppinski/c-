6e7b9ce73b5c0105ad52cd5431ed2e869e38127d
karppinski <krzysztof.karpinski135@gmail.com>
2024-08-09 11:17:24 +0200

    09.08

--- ParallelPrograming/Program.cs
+++ ParallelPrograming/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ParallelPrograming
+﻿using System.Net;
+
+namespace ParallelPrograming
 {
     internal class Program
     {
@@ -18,6 +20,46 @@
                 Console.Write(i + " ");
             }
 
+            IEnumerable<int> milion = Enumerable.Range(3, 1000000);
+
+            var cancelSource = new CancellationTokenSource();
+
+            var primeNumberQuery =
+                from n in milion.AsParallel().WithCancellation(cancelSource.Token)
+                where Enumerable.Range(2, (int)Math.Sqrt(n)).All(i => n % i > 0)
+                select n;
+
+            new Thread(() =>
+                {
+                    Thread.Sleep(100);
+                    cancelSource.Cancel();
+                }
+             ).Start();
+
+            try
+            {
+                //satrt query
+                int[] primes1 = primeNumberQuery.ToArray();
+                // we will never get here4 because the other thread will cancel us.
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Query canceled! ");
+            }
+
+            Parallel.ForEach("Hello, world", (c, loopState) =>
+           {
+               if (c == ',')
+                   loopState.Break();
+               else
+                   Console.WriteLine(c);
+           });
+
+            //Parallel.Invoke(
+            //    () => new WebClient().DownloadFile("http://linqpad.net", "lp/html"),
+            //    () => new WebClient().DownloadFile("mocrosoft.com", "ms.html"));
         }
+
     }
+    
 }
